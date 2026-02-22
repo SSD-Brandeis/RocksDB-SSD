4e92f854a8d8e2e2b83df1eae1f4497bf04b06b2
Shubham Kaushik <kaushiks@brandeis.edu>
2026-02-22 16:19:09 +0000

    minor

--- memtable/skiplistrep.cc
+++ memtable/skiplistrep.cc
@@ -142,19 +142,22 @@ public:
  }
 
 
- Status GetAndValidate(const LookupKey& k, void* callback_args,
-                       bool (*callback_func)(void* arg, const char* entry),
-                       bool allow_data_in_errors) override {
-   SkipListRep::Iterator iter(&skip_list_);
-   Slice dummy_slice;
-   Status status = iter.SeekAndValidate(dummy_slice, k.memtable_key().data(),
-                                        allow_data_in_errors);
-   for (; iter.Valid() && status.ok() &&
-          callback_func(callback_args, iter.key());
-        status = iter.NextAndValidate(allow_data_in_errors)) {
-   }
-   return status;
- }
+  Status GetAndValidate(const LookupKey& k, void* callback_args,
+                        bool (*callback_func)(void* arg, const char* entry),
+                        bool allow_data_in_errors, bool detect_key_out_of_order,
+                        const std::function<Status(const char*, bool)>&
+                            key_validation_callback) override {
+    SkipListRep::Iterator iter(&skip_list_);
+    Slice dummy_slice;
+    Status status = iter.SeekAndValidate(
+        dummy_slice, k.memtable_key().data(), allow_data_in_errors,
+        detect_key_out_of_order, key_validation_callback);
+    for (; iter.Valid() && status.ok() &&
+           callback_func(callback_args, iter.key());
+         status = iter.NextAndValidate(allow_data_in_errors)) {
+    }
+    return status;
+  }
 
 
  uint64_t ApproximateNumEntries(const Slice& start_ikey,
@@ -310,15 +313,21 @@ public:
    }
 
 
-   Status SeekAndValidate(const Slice& user_key, const char* memtable_key,
-                          bool allow_data_in_errors) override {
-     if (memtable_key != nullptr) {
-       return iter_.SeekAndValidate(memtable_key, allow_data_in_errors);
-     } else {
-       return iter_.SeekAndValidate(EncodeKey(&tmp_, user_key),
-                                    allow_data_in_errors);
-     }
-   }
+    Status SeekAndValidate(const Slice& user_key, const char* memtable_key,
+                           bool allow_data_in_errors,
+                           bool detect_key_out_of_order,
+                           const std::function<Status(const char*, bool)>&
+                               key_validation_callback) override {
+      if (memtable_key != nullptr) {
+        return iter_.SeekAndValidate(memtable_key, allow_data_in_errors,
+                                     detect_key_out_of_order,
+                                     key_validation_callback);
+      } else {
+        return iter_.SeekAndValidate(
+            EncodeKey(&tmp_, user_key), allow_data_in_errors,
+            detect_key_out_of_order, key_validation_callback);
+      }
+    }
 
 
    Status PrevAndValidate(bool allow_data_in_error) override {
